da062a58e4ae742168c1d6ee412c52e7ac92e323
Jack Greenhill <jackgreenhill@me.com>
2020-01-01 06:15:09 +1300

    Better code

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-import { timer } from 'rxjs';
-import { skipWhile, take } from 'rxjs/operators';
+import { timer, fromEvent } from 'rxjs';
+import { skipWhile, take, switchMap, tap } from 'rxjs/operators';
 import moment from 'moment';
 
-setTimeout(function() {
-  const video = document.querySelector('video');
-  const newYear = moment(`${new Date().getFullYear() + 1}-01-01T00:00:00`);
-  const startDate = moment(newYear).subtract(
-    moment.duration(64020)
-  );
+class VideoPlayScheduler {
+  constructor(finishTime) {
+    this.video = document.querySelector('video');
+    this.playTime = moment(finishTime).subtract(
+      moment.duration(64020)
+    );
+  }
 
-  timer(0, 10).pipe(
-    skipWhile(() => startDate > moment()),
-    take(1)
-  ).subscribe(() => {
-    video.play();
-  });
-}, 100);
+  schedule() {
+    return timer(0, 10).pipe(
+      skipWhile(() => this.playTime > moment()),
+      take(1),
+      tap(() => this.video.play())
+    );
+  }
+}
+
+const finishTime = `${new Date().getFullYear() + 1}-01-01T00:00:00`;
+
+fromEvent(window, 'DOMContentLoaded').pipe(
+  switchMap(() => new VideoPlayScheduler(finishTime).schedule())
+).subscribe(
+  () => console.log('Happy new year!')
+);
